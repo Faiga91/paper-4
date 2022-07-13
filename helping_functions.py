0685b73f61e2f21b8753aaec84e08da8b4cffc04
Faiga Alawad <faiga.alawad@ntnu.no>
2022-07-13 23:31:54 +0200

    refactor the codee

--- helping_functions.py
+++ helping_functions.py
@@ -3,6 +3,7 @@ A module that generate all the metrics for the policy.
 """
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+import torch
 
 def get_metrics(df1_, df2_):
     """
@@ -12,3 +13,21 @@ def get_metrics(df1_, df2_):
     nmae_ = mean_absolute_error(df1_, df2_, multioutput="uniform_average") / x_mean
     rmse_ = mean_squared_error(df1_,df2_, squared=False)
     return ([nmae_, rmse_])
+
+def MinMaxScaler(data):
+    """Min-Max Normalizer.
+    
+    Args:
+      - data: raw data
+      
+    Returns:
+      - norm_data: normalized data
+      - min_val: minimum values (for renormalization)
+      - max_val: maximum values (for renormalization)
+    """    
+    min_val = torch.min(data)
+    data = data - min_val
+    max_val = torch.max(data)
+    norm_data = data / (max_val + 1e-7)
+      
+    return norm_data, min_val, max_val
