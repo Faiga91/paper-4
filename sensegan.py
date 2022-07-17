56541059df2221f4e6826eb2f44dea018dc0d267
Faiga Alawad <faiga.alawad@ntnu.no>
2022-07-17 18:48:16 +0200

    Fixes

--- sensegan.py
+++ sensegan.py
@@ -5,30 +5,6 @@ import torch
 from torch import nn
 
 
-def get_generator_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
-    '''
-    Function for returning a block of the generator's neural network
-    given input and output dimensions.
-    Parameters:
-        input_dim: the dimension of the input vector, a scalar
-        output_dim: the dimension of the output vector, a scalar
-    Returns:
-        a generator neural network layer, with a linear transformation
-          followed by a batch normalization and then a relu activation
-    '''
-    if not final_layer:
-        return nn.Sequential(
-            nn.ConvTranspose1d(input_channels, output_channels, kernel_size, stride),
-            nn.BatchNorm1d(output_channels),
-            nn.ReLU()
-        )
-    else: # Final Layer
-        return nn.Sequential(
-            nn.ConvTranspose1d(input_channels, output_channels, kernel_size, stride),
-            nn.Tanh()
-        )
-    
-
 class Generator(nn.Module):
     '''
     Generator Class
@@ -94,110 +70,6 @@ def get_noise(n_samples, z_dim, device='cpu'):
     '''
     return torch.randn(n_samples, z_dim, device=device)
 
-def get_noise(n_samples, z_dim, device='cuda'):
-    '''
-    Function for creating noise vectors: Given the dimensions (n_samples, z_dim),
-    creates a tensor of that shape filled with random numbers from the normal distribution.
-    Parameters:
-        n_samples: the number of samples to generate, a scalar
-        z_dim: the dimension of the noise vector, a scalar
-        device: the device type
-    '''
-    # NOTE: To use this on GPU with device='cuda', make sure to pass the device
-    # argument to the function you use to generate the noise.
-    # pylint: disable=E1101
-    return  torch.randn(n_samples, z_dim, device = device)
-
-def get_discriminator_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
-    '''
-    Discriminator Block
-    Function for returning a neural network of the discriminator given input and output dimensions.
-    Parameters:
-        input_dim: the dimension of the input vector, a scalar
-        output_dim: the dimension of the output vector, a scalar
-    Returns:
-        a discriminator neural network layer, with a linear transformation
-          followed by an nn.LeakyReLU activation with negative slope of 0.2
-          (https://pytorch.org/docs/master/generated/torch.nn.LeakyReLU.html)
-    '''
-    if not final_layer:
-        return nn.Sequential(
-            nn.Conv1d(input_channels, output_channels, kernel_size, stride),
-            nn.BatchNorm1d(output_channels),
-            nn.LeakyReLU(negative_slope=0.2)
-        )
-    else: # Final Layer
-        return nn.Sequential(
-            nn.Conv1d(input_channels, output_channels, kernel_size, stride)
-        )
-
-class Discriminator(nn.Module):
-    '''
-    Discriminator Class
-    Values:
-        in_dim: the dimension of the generated variable, fitted for the dataset used, a scalar
-        hidden_dim: the inner dimension, a scalar
-    '''
-    def __init__(self, in_dim=1, hidden_dim=128):
-        super().__init__()
-        self.disc = nn.Sequential(
-            get_discriminator_block(in_dim, hidden_dim * 4),
-            get_discriminator_block(hidden_dim * 4, hidden_dim * 2),
-            get_discriminator_block(hidden_dim * 2, hidden_dim),
-            nn.Linear(hidden_dim, 1)
-        )
-
-    def forward(self, input):
-        '''
-        Function for completing a forward pass of the discriminator: Given an input tensor,
-        returns a 1-dimension tensor representing fake/real.
-        Parameters:
-            input: a flattened input tensor with dimension (in_dim)
-        '''
-        return self.disc(input)
-
-    # Needed for grading
-    def get_disc(self):
-        '''
-        Returns:
-            the sequential model
-        '''
-        return self.disc
-
-# pylint: disable=R0913
-def get_disc_loss(gen, disc, criterion, real, num_inputs, z_dim, device):
-    '''
-    Return the loss of the discriminator given inputs.
-    Parameters:
-        gen: the generator model, which returns an input given z-dimensional noise
-        disc: the discriminator model, which returns a single-dimensional prediction of real/fake
-        criterion: the loss function, which should be used to compare
-               the discriminator's predictions to the ground truth reality of the inputs
-               (e.g. fake = 0, real = 1)
-        real: a batch of real inputs
-        num_inputs: the number of inputs the generator should produce,
-                which is also the length of the real inputs
-        z_dim: the dimension of the noise vector, a scalar
-        device: the device type
-    Returns:
-        disc_loss: a torch scalar loss value for the current batch
-    '''
-
-    fake = gen(get_noise(num_inputs, z_dim, device=device)).detach()
-    pred_f = disc(fake)
-    pred_r = disc(real)
-    # pylint: disable=E1101
-    ground_truth_f = torch.torch.zeros_like(pred_f)
-    ground_truth_r = torch.torch.ones_like(pred_r)
-
-    loss_f = criterion(pred_f, ground_truth_f)
-    loss_r = criterion(pred_r, ground_truth_r)
-
-    disc_loss = (loss_f + loss_r) / 2
-
-    return disc_loss
-
-
 def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
     '''
     Return the loss of a critic given the critic's scores for fake and real images,
@@ -229,6 +101,61 @@ def get_gen_loss(crit_fake_pred):
     #### END CODE HERE ####
     return gen_loss
 
+def get_gradient(crit, real, fake, epsilon):
+    '''
+    Return the gradient of the critic's scores with respect to mixes of real and fake images.
+    Parameters:
+        crit: the critic model
+        real: a batch of real images
+        fake: a batch of fake images
+        epsilon: a vector of the uniformly random proportions of real/fake per mixed image
+    Returns:
+        gradient: the gradient of the critic's scores, with respect to the mixed image
+    '''
+    # Mix the images together
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+
+    # Calculate the critic's scores on the mixed images
+    mixed_scores = crit(mixed_images)
+    
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        # Note: You need to take the gradient of outputs with respect to inputs.
+        # This documentation may be useful, but it should not be necessary:
+        # https://pytorch.org/docs/stable/autograd.html#torch.autograd.grad
+        #### START CODE HERE ####
+        inputs= mixed_images, 
+        outputs= mixed_scores,
+        #### END CODE HERE ####
+        # These other parameters have to do with the pytorch autograd engine works
+        grad_outputs=torch.ones_like(mixed_scores), 
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    return gradient
+
+
+def gradient_penalty(gradient):
+    '''
+    Return the gradient penalty, given a gradient.
+    Given a batch of image gradients, you calculate the magnitude of each image's gradient
+    and penalize the mean quadratic distance of each magnitude to 1.
+    Parameters:
+        gradient: the gradient of the critic's scores, with respect to the mixed image
+    Returns:
+        penalty: the gradient penalty
+    '''
+    # Flatten the gradients so that each row captures one image
+    gradient = gradient.view(len(gradient), -1)
+
+    # Calculate the magnitude of every row
+    gradient_norm = gradient.norm(2, dim=1)
+    
+    # Penalize the mean squared distance of the gradient norms from 1
+    #### START CODE HERE ####
+    penalty = torch.mean((1 - gradient_norm) ** 2)
+    #### END CODE HERE ####
+    return penalty
 
 class Critic(nn.Module):
     '''
