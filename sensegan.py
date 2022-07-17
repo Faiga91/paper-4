01a0d69d3f7311ba2472db6d045460aab7d168ba
Faiga Alawad <faiga.alawad@ntnu.no>
2022-07-17 15:31:03 +0200

    change the image to input

--- sensegan.py
+++ sensegan.py
@@ -27,11 +27,10 @@ class Generator(nn.Module):
     Generator Class
     Values:
         z_dim: the dimension of the noise vector, a scalar
-        im_dim: the dimension of the images, fitted for the dataset used, a scalar
-          (MNIST images are 28 x 28 = 784 so that is your default)
+        in_dim: the dimension of the input
         hidden_dim: the inner dimension, a scalar
     '''
-    def __init__(self, z_dim=10, im_dim=1, hidden_dim=128):
+    def __init__(self, z_dim=10, in_dim=1, hidden_dim=128):
         super().__init__()
         # Build the neural network
         self.gen = nn.Sequential(
@@ -40,14 +39,14 @@ class Generator(nn.Module):
             get_generator_block(hidden_dim * 2, hidden_dim * 4),
             get_generator_block(hidden_dim * 4, hidden_dim * 8),
 
-            nn.Linear(hidden_dim * 8, im_dim),
+            nn.Linear(hidden_dim * 8, in_dim),
             nn.Sigmoid()
 
         )
     def forward(self, noise):
         '''
         Function for completing a forward pass of the generator: Given a noise tensor,
-        returns generated images.
+        returns generated time-series.
         Parameters:
             noise: a noise tensor with dimensions (n_samples, z_dim)
         '''
@@ -96,27 +95,26 @@ class Discriminator(nn.Module):
     '''
     Discriminator Class
     Values:
-        im_dim: the dimension of the images, fitted for the dataset used, a scalar
-            (MNIST images are 28x28 = 784 so that is your default)
+        in_dim: the dimension of the generated variable, fitted for the dataset used, a scalar
         hidden_dim: the inner dimension, a scalar
     '''
-    def __init__(self, im_dim=1, hidden_dim=128):
+    def __init__(self, in_dim=1, hidden_dim=128):
         super().__init__()
         self.disc = nn.Sequential(
-            get_discriminator_block(im_dim, hidden_dim * 4),
+            get_discriminator_block(in_dim, hidden_dim * 4),
             get_discriminator_block(hidden_dim * 4, hidden_dim * 2),
             get_discriminator_block(hidden_dim * 2, hidden_dim),
             nn.Linear(hidden_dim, 1)
         )
 
-    def forward(self, image):
+    def forward(self, input):
         '''
-        Function for completing a forward pass of the discriminator: Given an image tensor,
+        Function for completing a forward pass of the discriminator: Given an input tensor,
         returns a 1-dimension tensor representing fake/real.
         Parameters:
-            image: a flattened image tensor with dimension (im_dim)
+            input: a flattened input tensor with dimension (in_dim)
         '''
-        return self.disc(image)
+        return self.disc(input)
 
     # Needed for grading
     def get_disc(self):
@@ -127,25 +125,25 @@ class Discriminator(nn.Module):
         return self.disc
 
 # pylint: disable=R0913
-def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
+def get_disc_loss(gen, disc, criterion, real, num_inputs, z_dim, device):
     '''
     Return the loss of the discriminator given inputs.
     Parameters:
-        gen: the generator model, which returns an image given z-dimensional noise
+        gen: the generator model, which returns an input given z-dimensional noise
         disc: the discriminator model, which returns a single-dimensional prediction of real/fake
         criterion: the loss function, which should be used to compare
-               the discriminator's predictions to the ground truth reality of the images
+               the discriminator's predictions to the ground truth reality of the inputs
                (e.g. fake = 0, real = 1)
-        real: a batch of real images
-        num_images: the number of images the generator should produce,
-                which is also the length of the real images
+        real: a batch of real inputs
+        num_inputs: the number of inputs the generator should produce,
+                which is also the length of the real inputs
         z_dim: the dimension of the noise vector, a scalar
         device: the device type
     Returns:
         disc_loss: a torch scalar loss value for the current batch
     '''
 
-    fake = gen(get_noise(num_images, z_dim, device=device)).detach()
+    fake = gen(get_noise(num_inputs, z_dim, device=device)).detach()
     pred_f = disc(fake)
     pred_r = disc(real)
     # pylint: disable=E1101
@@ -160,23 +158,23 @@ def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
     return disc_loss
 
 
-def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
+def get_gen_loss(gen, disc, criterion, num_inputs, z_dim, device):
     '''
     Return the loss of the generator given inputs.
     Parameters:
-        gen: the generator model, which returns an image given z-dimensional noise
+        gen: the generator model, which returns an input given z-dimensional noise
         disc: the discriminator model, which returns a single-dimensional prediction of real/fake
         criterion: the loss function, which should be used to compare
-               the discriminator's predictions to the ground truth reality of the images
+               the discriminator's predictions to the ground truth reality of the inputs
                (e.g. fake = 0, real = 1)
-        num_images: the number of images the generator should produce,
-                which is also the length of the real images
+        num_inputs: the number of inputs the generator should produce,
+                which is also the length of the real inputs
         z_dim: the dimension of the noise vector, a scalar
         device: the device type
     Returns:
         gen_loss: a torch scalar loss value for the current batch
     '''
-    noise = get_noise(num_images, z_dim, device=device)
+    noise = get_noise(num_inputs, z_dim, device=device)
     fake = gen(noise)
     pred_f = disc(fake)
     # pylint: disable=E1101
