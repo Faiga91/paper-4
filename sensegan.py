4518e71a49feb1e1df70bf9d038e534b8fe53950
Faiga Alawad <faiga.alawad@ntnu.no>
2022-07-18 12:11:25 +0200

    Remove the kernel size and the stride

--- sensegan.py
+++ sensegan.py
@@ -20,32 +20,30 @@ class Generator(nn.Module):
         # Build the neural network
         self.gen = nn.Sequential(
             self.make_gen_block(z_dim, hidden_dim * 4),
-            self.make_gen_block(hidden_dim * 4, hidden_dim * 2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2),
             self.make_gen_block(hidden_dim * 2, hidden_dim),
-            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+            self.make_gen_block(hidden_dim, im_chan, final_layer=True),
         )
 
-    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+    def make_gen_block(self, input_channels, output_channels, final_layer=False):
         '''
         Function to return a sequence of operations corresponding to a generator block of DCGAN;
         a transposed convolution, a batchnorm (except in the final layer), and an activation.
         Parameters:
             input_channels: how many channels the input feature representation has
             output_channels: how many channels the output feature representation should have
-            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
-            stride: the stride of the convolution
             final_layer: a boolean, true if it is the final layer and false otherwise 
                       (affects activation and batchnorm)
         '''
         if not final_layer:
             return nn.Sequential(
-                nn.ConvTranspose1d(input_channels, output_channels, kernel_size, stride),
+                nn.ConvTranspose1d(input_channels, output_channels),
                 nn.BatchNorm1d(output_channels),
                 nn.ReLU(inplace=True),
             )
         else:
             return nn.Sequential(
-                nn.ConvTranspose1d(input_channels, output_channels, kernel_size, stride),
+                nn.ConvTranspose1d(input_channels, output_channels),
                 nn.Tanh(),
             )
 
@@ -173,27 +171,25 @@ class Critic(nn.Module):
             self.make_crit_block(hidden_dim * 2, 1, final_layer=True),
         )
 
-    def make_crit_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+    def make_crit_block(self, input_channels, output_channels, final_layer=False):
         '''
         Function to return a sequence of operations corresponding to a critic block of DCGAN;
         a convolution, a batchnorm (except in the final layer), and an activation (except in the final layer).
         Parameters:
             input_channels: how many channels the input feature representation has
             output_channels: how many channels the output feature representation should have
-            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
-            stride: the stride of the convolution
             final_layer: a boolean, true if it is the final layer and false otherwise 
                       (affects activation and batchnorm)
         '''
         if not final_layer:
             return nn.Sequential(
-                nn.Conv1d(input_channels, output_channels, kernel_size, stride),
+                nn.Conv1d(input_channels, output_channels),
                 nn.BatchNorm1d(output_channels),
                 nn.LeakyReLU(0.2, inplace=True),
             )
         else:
             return nn.Sequential(
-                nn.Conv1d(input_channels, output_channels, kernel_size, stride),
+                nn.Conv1d(input_channels, output_channels),
             )
 
     def forward(self, image):
